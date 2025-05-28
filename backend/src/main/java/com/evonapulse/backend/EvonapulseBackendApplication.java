49c7db704621a6f20a4541e435e028fae73d3316
Jbristhuille <jbristhuille@gmail.com>
2025-05-28 14:40:17 +0200

    Setup empty springboot project

---
+++ backend/src/main/java/com/evonapulse/backend/EvonapulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.evonapulse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvonapulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvonapulseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/evonapulse/backend/EvonapulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evonapulse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvonapulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
