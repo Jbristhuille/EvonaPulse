cc8f767708dcec7778fcf2ec8abfd33a284eaacf
Jbristhuille <jbristhuille@gmail.com>
2025-06-02 15:40:13 +0200

    add metric repository

---
+++ backend/src/main/java/com/evonapulse/backend/repositories/MetricRepository.java
@@ -0,0 +1,9 @@
+package com.evonapulse.backend.repositories;
+
+import com.evonapulse.backend.entities.MetricEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface MetricRepository extends JpaRepository<MetricEntity, UUID> {
+    boolean existsByNameAndProject_Id(String name, UUID projectId);
+}
