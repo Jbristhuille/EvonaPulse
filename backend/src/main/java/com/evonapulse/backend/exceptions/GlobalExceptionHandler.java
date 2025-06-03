2294fb21f700a94b53b4fc8632a27ab6b4078201
Jbristhuille <jbristhuille@gmail.com>
2025-06-03 16:37:17 +0200

    feat(errors): improve error handling for invalid or missing path variables

--- backend/src/main/java/com/evonapulse/backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/evonapulse/backend/exceptions/GlobalExceptionHandler.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -36,6 +38,24 @@ public class GlobalExceptionHandler {
                 .orElse("Validation failed");
 
         ApiErrorBuilder error = new ApiErrorBuilder(HttpStatus.BAD_REQUEST, message, request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiErrorBuilder> handleTypeMismatch(MethodArgumentTypeMismatchException ex, HttpServletRequest request) {
+        String param = ex.getName();
+        String value = String.valueOf(ex.getValue());
+        String msg = "Invalid value for parameter '" + param + "': '" + value + "'";
+
+        ApiErrorBuilder error = new ApiErrorBuilder(HttpStatus.BAD_REQUEST, msg, request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MissingPathVariableException.class)
+    public ResponseEntity<ApiErrorBuilder> handleMissingPathVariable(MissingPathVariableException ex, HttpServletRequest request) {
+        String msg = "Missing path variable: " + ex.getVariableName();
+
+        ApiErrorBuilder error = new ApiErrorBuilder(HttpStatus.BAD_REQUEST, msg, request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
